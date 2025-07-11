2d4cbdd6ce87b483df7c26c811409403be87ff7a
sehun-stat <sahoon1004@gmail.com>
2025-07-11 16:10:52 +0900

    update .tar.gz

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -8,6 +8,7 @@
 #' @param active_dim a num
 #' 
 #' @return a matrix powered by alpha
+#' @export
 matpower_1 <- function(a, alpha, active_dim = 1e-6) {
     .Call(`_FAGLasso_matpower_1`, a, alpha, active_dim)
 }
@@ -27,6 +28,7 @@ matpower_1 <- function(a, alpha, active_dim = 1e-6) {
 #'    \item \code{Sigma}:  list object with \code{Sigma[[j]]} is the coordinate representation of j-th covariance operator, whose basis system is eigenfunctions.
 #'    \item \code{non_centered}: non_centered design matrix based on eigenfunctions.
 #'  }
+#' @export
 fpca_cpp <- function(coefs, GB) {
     .Call(`_FAGLasso_fpca_cpp`, coefs, GB)
 }
@@ -34,6 +36,7 @@ fpca_cpp <- function(coefs, GB) {
 #' Soft thresh function
 #' @param x a numeric number.
 #' @return \code{x * I(x > 0)}.
+#' @export
 soft_thresh <- function(x) {
     .Call(`_FAGLasso_soft_thresh`, x)
 }
@@ -52,6 +55,7 @@ soft_thresh <- function(x) {
 #' @param active_dim a threshold for inverse of matrix.
 #' @param max_iter the maximum number of iterations allowed.
 #' @param standardnorm If set to 'TRUE', the standard L2 norm is employed instead of our gamma norm. Note that it is just for the simulation study.
+#' @export
 f_agL <- function(y, design, cov_mat, basis_num, weights, lambda_1, lambda_2, Ps, initN, centered = FALSE, active_dim = 1e-6, max_iter = 1000L, standardnorm = FALSE) {
     .Call(`_FAGLasso_f_agL`, y, design, cov_mat, basis_num, weights, lambda_1, lambda_2, Ps, initN, centered, active_dim, max_iter, standardnorm)
 }
